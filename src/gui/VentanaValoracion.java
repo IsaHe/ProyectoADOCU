7e9ea3f1accc0d9d5fad4e5b9a619797fad3b369
Isaac Herbozo <isaacherbozo06@gmail.com>
2023-10-19 08:53:09 +0200

    Mejoras en el formato del texto y adicion del boton para enviar la valoracion

--- src/gui/VentanaValoracion.java
+++ src/gui/VentanaValoracion.java
@@ -9,48 +9,42 @@ public class VentanaValoracion extends JFrame {
         MUY_MAL, MAL, REGULAR, BIEN, MUY_BIEN
     }
     public VentanaValoracion() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         setTitle("Valoración");
-        setIconImage(new ImageIcon("C:\\Users\\PC\\IdeaProjects\\ProyectoADOCU\\src\\resources\\images\\ADOCU.png").getImage());
-
-        JLabel etiqueta = new JLabel("Valoración: ");
+        setIconImage(new ImageIcon(Objects.requireNonNull(getClass().getResource("/resources/images//ADOCU.png"))).getImage());
 
         JComboBox<Valoracion> valoracion = new JComboBox<>(Valoracion.values());
 
-        JLabel etiqueta2 = new JLabel("<html>Elige una valoración que creas adecuada<br>basandote en tu experiencia con el producto</html>");
+        String htmlContent = "<html><body>" +
+                "<div style='text-align: center; margin-top: 20px;'>" +
+                "<img src='file:///C:/Users/Thega/IdeaProjects/ProyectoADOCU/src/resources/images/ADOCU.png' alt='Logo de ADOCU' width='150'><br><br>" +
+                "<h2>¡Gracias por elegir nuestros espacios para tu actividad!</h2>" +
+                "<p>En [Nombre de la Empresa], nos esforzamos por brindarte la mejor experiencia en nuestros espacios. Para seguir mejorando y ofrecerte un servicio excepcional, te pedimos que tomes un momento para valorar tu experiencia con nosotros.</p>" +
+                "<p>Tu opinión es invaluable para nosotros. Nos ayuda a entender lo que estamos haciendo bien y a identificar áreas en las que podemos mejorar. Estamos comprometidos en proporcionarte un entorno cómodo y acogedor para tus actividades, y tu feedback nos guía en ese camino.</p>" +
+                "<p>Por favor, haz clic en el enlace a continuación para completar nuestra breve encuesta de valoración. Solo tomará unos minutos de tu tiempo y nos ayudará a seguir ofreciendo un servicio de calidad.</p>" +
+                "<a href='enlace_a_encuesta' style='text-decoration: none; background-color: #4CAF50; color: white; padding: 10px 20px; border-radius: 5px;'>Completar la Encuesta</a>" +
+                "<p>Gracias por tu colaboración y por confiar en [Nombre de la Empresa]. Esperamos verte de nuevo pronto en nuestros espacios.</p>" +
+                "<p>Atentamente,<br>El equipo de [Nombre de la Empresa]</p>" +
+                "</div></body></html>";
+        JEditorPane etiqueta = new JEditorPane("text/html", htmlContent);
+        etiqueta.setEditable(false);
+        etiqueta.setOpaque(false);
 
-        valoracion.addActionListener(e -> {
-            etiqueta2.setText(
-                    "<html>Elige una valoración que creas adecuada<br>" +
-                    "basandote en tu experiencia con el producto<br>" +
-                    "Teniendo en cuenta que 1 es la peor y 5 la mejor.<br>" +
-                    "Actualmente la valoracion que has seleccionado equivale a:<br>"+
-                    Objects.requireNonNull(valoracion.getSelectedItem()) +
-                    "</html>");
-            Valoracion v = (Valoracion) valoracion.getSelectedItem();
-            switch (v) {
-                case MUY_MAL:
-                    etiqueta.setForeground(Color.RED);
-                    break;
-                case MAL:
-                    etiqueta.setForeground(Color.ORANGE);
-                    break;
-                case REGULAR:
-                    etiqueta.setForeground(Color.YELLOW);
-                    break;
-                case BIEN:
-                    etiqueta.setForeground(Color.GREEN);
-                    break;
-                case MUY_BIEN:
-                    etiqueta.setForeground(Color.BLUE);
-                    break;
-            }
-        });
-        add(etiqueta, BorderLayout.WEST);
-        add(valoracion, BorderLayout.CENTER);
-        add(etiqueta2, BorderLayout.EAST);
+        JPanel panelValoracion = new JPanel();
+        panelValoracion.setLayout(new BorderLayout());
 
-        pack();
+        JButton btnEnviar = new JButton("Enviar");
+        btnEnviar.addActionListener(e -> dispose());
+
+        panelValoracion.add(valoracion, BorderLayout.CENTER);
+        panelValoracion.add(btnEnviar, BorderLayout.SOUTH);
+
+        add(panelValoracion, BorderLayout.SOUTH);
+        add(etiqueta, BorderLayout.CENTER);
+
+        setSize(1014, 607);
+        setResizable(false);
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 
