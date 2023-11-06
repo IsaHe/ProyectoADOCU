6203804ef40c0d64563f02176a4c94c730ffc01d
Aitor Peral <145749077+PeralAitor@users.noreply.github.com>
2023-11-06 21:03:35 +0100

    Ventana Pago Sin Funcionalidad

--- src/gui/VentanaPago.java
+++ src/gui/VentanaPago.java
@@ -10,10 +10,12 @@ public class VentanaPago extends JFrame{
 	private static final long serialVersionUID = 1L;
 
 	private final Logger logger = Logger.getLogger(VentanaPago.class.getName());
-	private JPanel pCentro, pSur, pNorte, pNumTarjeta, pImporte, pCVV, pAbajoUsuario, pAbajoContraseña, pUsuario, pContra;
-	private JLabel lblImporte, lblNumTarjeta, lblCVV, lblTitulo, lblUsuario, lblContrasena;
-	private JTextField txtTitular, txtNumTarjeta, txtCVV, txtUsuario;
-	private JButton btnPagar, btnVolver;
+	private DefaultListModel<String> modeloPendiente, modeloPagada;
+	private JList<String> listaPendiente, listaPagada;
+	private JPanel pCentro, pSur, pNorte, pCentroArriba, pCentroAbajo, pUsuario, pContraseña, pEntreListas, pPagar, pTodosPagar, pPendiente,pTodosPendiente , pImporte;
+	private JLabel lblImporte, lblTitulo, lblUsuario, lblContrasena, lblImporteAPagar;
+	private JTextField txtUsuario;
+	private JButton btnPagar, btnVolver, btnPasarAPagada, btnPasarAPendiente, btnPasarTodosAPagar, btnPasarTodosAPendiente;
 	private JPasswordField pasContrasena;
 
 	public VentanaPago(){
@@ -25,52 +27,58 @@ public class VentanaPago extends JFrame{
 		
 		//CREACION CONTENEDORES
 		pCentro = new JPanel();
+		pCentroArriba = new JPanel();
+		pEntreListas = new JPanel();
+		pPagar = new JPanel();
+		pTodosPagar = new JPanel();
+		pPendiente = new JPanel();
+		pTodosPendiente = new JPanel();
+		pImporte = new JPanel();
+		pCentroAbajo = new JPanel();
+		pUsuario = new JPanel();
+		pContraseña = new JPanel();
 		pSur = new JPanel();
 		pNorte = new JPanel();
-		pImporte = new JPanel();
-		pNumTarjeta = new JPanel();
-		pCVV = new JPanel();
-		pAbajoUsuario = new JPanel();
-		pAbajoContraseña = new JPanel();
-		pUsuario = new JPanel();
-		pContra = new JPanel();
 		
 		//FUNCION CONTENEDORES
-		pCentro.setLayout(new GridLayout(0,2));
-		pSur.setLayout(new GridLayout(0,2));
-		pImporte.setLayout(new GridLayout(3,1));
-		pNumTarjeta.setLayout(new GridLayout(3,1));
-		pCVV.setLayout(new GridLayout(3,1));
-		pAbajoUsuario.setLayout(new GridLayout(0,2));
-		pAbajoContraseña.setLayout(new GridLayout(0,2));
-		pUsuario.setLayout(new GridLayout(3,0));
-		pContra.setLayout(new GridLayout(3,0));
+		pCentro.setLayout(new GridLayout(2, 1));
+		pCentroArriba.setLayout(new GridLayout(1,3));
+		pEntreListas.setLayout(new GridLayout(5,1));
+		pImporte.setLayout(new GridLayout(1,2));
+		pCentroAbajo.setLayout(new GridLayout(1,4));
+		pUsuario.setLayout(new GridLayout(5, 1));
+		pContraseña.setLayout(new GridLayout(5, 1));
+		pSur.setLayout(new GridLayout(1, 2));
 		
 		//AÑADIR CONTENEDOR A VENTANA PRINCIPAL
-		getContentPane().add(pCentro, BorderLayout.CENTER);
 		getContentPane().add(pSur, BorderLayout.SOUTH);
 		getContentPane().add(pNorte, BorderLayout.NORTH);
+		getContentPane().add(pCentro, BorderLayout.CENTER);
 		
 		//CREACION COMPONENTES
 		lblTitulo = new JLabel("PAGOS");
-		lblImporte = new JLabel("IMPORTE A PAGAR: ");
-		lblNumTarjeta = new JLabel("NUMERO DE LA TARJETA: ");
-		lblCVV = new JLabel("CVV: ");
-		lblUsuario = new JLabel("USUARIO: ");
-		lblContrasena = new JLabel("CONTRASEÑA: ");
-		txtTitular = new JTextField(20);
-		txtNumTarjeta = new JTextField(20);
-		txtCVV = new JTextField(20);
+		lblImporte = new JLabel("    - Importe a Pagar: ");
+		lblImporteAPagar = new JLabel("0€");
+		lblUsuario = new JLabel("    - Usuario: ");
 		txtUsuario = new JTextField(20);
+		lblContrasena = new JLabel("    - Contraseña: ");
 		pasContrasena = new JPasswordField(20);
+		pasContrasena = new JPasswordField();
 		btnPagar = new JButton("PAGAR");
 		btnVolver = new JButton("VOLVER");
+		modeloPendiente = new DefaultListModel<>();
+		listaPendiente = new JList<>(modeloPendiente);
+		modeloPagada = new DefaultListModel<>();
+		listaPagada = new JList<>(modeloPagada);
+		btnPasarAPagada = new JButton(">");
+		btnPasarAPendiente = new JButton("<");
+		btnPasarTodosAPagar = new JButton(">>");
+		btnPasarTodosAPendiente = new JButton("<<");
 		
 		//FUNCION COMPONENTES
 		lblTitulo.setFont(new Font(Font.SERIF, Font.PLAIN, 60));
-		lblImporte.setFont(new Font(Font.SERIF, Font.PLAIN, 35));
-		lblNumTarjeta.setFont(new Font(Font.SERIF, Font.PLAIN, 35));
-		lblCVV.setFont(new Font(Font.SERIF, Font.PLAIN, 35));
+		lblImporte.setFont(new Font(Font.SERIF, Font.PLAIN, 20));
+		lblImporteAPagar.setFont(new Font(Font.SERIF, Font.BOLD, 16));
 		lblUsuario.setFont(new Font(Font.SERIF, Font.PLAIN, 35));
 		lblContrasena.setFont(new Font(Font.SERIF, Font.PLAIN, 35));
 		btnPagar.setFont(new Font(Font.SERIF, Font.PLAIN, 30));
@@ -78,35 +86,38 @@ public class VentanaPago extends JFrame{
 		
 		//AÑADIR COMPONENTES A CONTENEDORES
 		pNorte.add(lblTitulo);
-		pSur.add(btnVolver);
-		pSur.add(btnPagar);
-		pCentro.add(lblImporte);
-		pCentro.add(pImporte);
-		pImporte.add(new JPanel());
-		pImporte.add(txtTitular);
-		pImporte.add(new JPanel());
-		pCentro.add(lblNumTarjeta);
-		pCentro.add(pNumTarjeta);
-		pNumTarjeta.add(new JPanel());
-		pNumTarjeta.add(txtNumTarjeta);
-		pNumTarjeta.add(new JPanel());
-		pCentro.add(lblCVV);
-		pCentro.add(pCVV);
-		pCVV.add(new JPanel());
-		pCVV.add(txtCVV);
-		pCVV.add(new JPanel());
-		pCentro.add(pAbajoUsuario);
-		pCentro.add(pAbajoContraseña);
-		pAbajoUsuario.add(lblUsuario);
-		pAbajoContraseña.add(lblContrasena);
-		pAbajoUsuario.add(pUsuario);
-		pAbajoContraseña.add(pContra);
+		pCentro.add(pCentroArriba);
+		pCentro.add(pCentroAbajo);
+		pCentroArriba.add(listaPendiente);
+		pCentroArriba.add(pEntreListas);
+		pEntreListas.add(pPagar);
+		pPagar.add(btnPasarAPagada);
+		pEntreListas.add(pTodosPagar);
+		pTodosPagar.add(btnPasarTodosAPagar);
+		pEntreListas.add(pPendiente);
+		pPendiente.add(btnPasarAPendiente);
+		pEntreListas.add(pTodosPendiente);
+		pTodosPendiente.add(btnPasarTodosAPendiente);
+		pEntreListas.add(pImporte);
+		pImporte.add(lblImporte);
+		pImporte.add(lblImporteAPagar);
+		pCentroArriba.add(listaPagada);
+		pCentroAbajo.add(lblUsuario);
+		pCentroAbajo.add(pUsuario);
+		pUsuario.add(new JPanel());
 		pUsuario.add(new JPanel());
 		pUsuario.add(txtUsuario);
 		pUsuario.add(new JPanel());
-		pContra.add(new JPanel());
-		pContra.add(pasContrasena);
-		pContra.add(new JPanel());
+		pUsuario.add(new JPanel());
+		pCentroAbajo.add(lblContrasena);
+		pCentroAbajo.add(pContraseña);
+		pContraseña.add(new JPanel());
+		pContraseña.add(new JPanel());
+		pContraseña.add(pasContrasena);
+		pContraseña.add(new JPanel());
+		pContraseña.add(new JPanel());
+		pSur.add(btnPagar);
+		pSur.add(btnVolver);
 		
 		//EVENTOS BOTONES
 		btnVolver.addActionListener((e)->{
