552a80339b7528821a1985f4b5fea99c589eae56
Isaac Esteban <isaacherbozo06@gmail.com>
2023-11-18 12:36:42 +0100

    Añadidas las clases y recursos para los test(vacios)

---
+++ Test/db/GestorFicherosTest.java
@@ -0,0 +1,40 @@
+package db;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GestorFicherosTest {
+
+    @Test
+    public void obtenerActividadesDeFichero() {
+    }
+
+    @Test
+    public void cargarActividadesSemanalesEnFichero() {
+    }
+
+    @Test
+    public void obtenerActividadesSemanalesDeFichero() {
+    }
+
+    @Test
+    public void getActividades() {
+    }
+
+    @Test
+    public void getActividadesSemanales() {
+    }
+
+    @Test
+    public void setActividad() {
+    }
+
+    @Test
+    public void setUltimoAccesoAtabla() {
+    }
+
+    @Test
+    public void actualizarActividadesSemanales() {
+    }
+}
---
+++ Test/domain/ActividadTest.java
@@ -0,0 +1,48 @@
+package domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ActividadTest {
+
+    @Test
+    public void getPrecio() {
+    }
+
+    @Test
+    public void setPrecio() {
+    }
+
+    @Test
+    public void getNombre() {
+    }
+
+    @Test
+    public void setNombre() {
+    }
+
+    @Test
+    public void getNumeroParticipantes() {
+    }
+
+    @Test
+    public void setNumeroParticipantes() {
+    }
+
+    @Test
+    public void toStringBd() {
+    }
+
+    @Test
+    public void shiftArray() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+}
